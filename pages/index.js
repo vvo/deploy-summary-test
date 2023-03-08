f6fb8630e4d3f5240f4e94264a83fa6f98c338d6
Vincent Voyer <vincent@codeagain.com>
2023-03-08 11:56:01 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -59,12 +59,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div
-          className={styles.grid}
-          style={{
-            background: "red",
-          }}
-        >
+        <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
